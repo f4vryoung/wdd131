01967cb19755794d07f49ca5f6c641d31b3fc4eb
f4vryoung <foreveryounggenealogy@gmail.com>
2025-05-02 11:09:49 -0600

    Week 2 Changes

---
+++ mission/mission.js
@@ -0,0 +1,32 @@
+const themeSelector = document.querySelector("#theme-select")
+const body = document.body;
+const logo = document.querySelector(".logo");
+
+function changeTheme() {
+    const currentTheme = themeSelector.value;
+
+    if (currentTheme === "dark") {
+        body.classList.add("dark");
+        if (logo) {
+            logo.src = "byui-logo_white.png";
+        }
+    } else {
+        body.classList.remove("dark");
+        if (logo) {
+            logo.src = "byui-logo_blue.webp"
+        }
+    }
+// check to see what the current value of our select is.
+// The current value is conveniently found in themeSelector.value!
+
+// if the value is dark then:
+// add the dark class to the body
+// change the source of the logo img to point to the white logo.
+// otherwise
+// remove the dark class
+// make sure the logo src is the blue logo.
+}
+
+// add an event listener to the themeSelector element here.
+// Use the changeTheme function as the event handler function.
+themeSelector.addEventListener('change', changeTheme);
